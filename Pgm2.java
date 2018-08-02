563797bbcd4b063548f46381aff7bed433e538c7
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-02 11:16:15 +0530

    Add files via upload

---
+++ Pgm2.java
@@ -0,0 +1,17 @@
+package guviplayset123;
+import java.util.*;
+public class Pgm2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int fact=1;
+for(int i=1;i<=n;i++)
+{
+	fact*=i;
+}
+System.out.println(fact);
+	}
+
+}
