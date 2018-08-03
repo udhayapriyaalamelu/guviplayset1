8449ebb9a8985955e37ff164a30103760eac3fcc
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-03 10:25:26 +0530

    Create Pgm20.java

---
+++ Pgm20.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.Scanner;
+class Pgm20
+{
+public static void main(String []args)
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.next.Line();
+for(int i=0;i<s.length();i++)
+{
+  if(s.charAt(i)=='z')
+  System.out.print("c");
+  else if(s.charAt(i)=='Z')
+  System.out.print("C");
+  else if(s.charAt(i)=='y')
+  System.out.print("b");
+  else if(s.charAt(i)=='Y')
+  System.out.print("B");
+  else if(s.charAt(i)=='x')
+  System.out.print("a");
+  else if(s.charAt(i)=='X')
+  System.out.print("A");
+  else
+  System.out.print((char)s.charAt(i)+3);
+  }
+  }
+  }
